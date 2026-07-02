89575498a25f1d024248b685e04e332ad235dec7
DanyRAAD <ramirezandresdaniela@gmail.com>
2026-07-01 20:41:06 -0600

    fix config con site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://danyraad.github.io",
+
   vite: {
     plugins: [tailwindcss()],
   },
